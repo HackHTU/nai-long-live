65dcfbfd06e0e9287a74079b6b66ae9351be0c6f
anonymous <anonymous>
2026-06-15 13:47:16 +0800

    chore: add tooling and clean starter app

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from "node:url";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue(), vueDevTools(), cloudflare()],
+	plugins: [vue(), vueDevTools(), tailwindcss(), cloudflare()],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
